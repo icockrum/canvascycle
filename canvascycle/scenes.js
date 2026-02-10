41c7c8e0d4f59ff0212aba12643dcd55ae8e4be8
Ivan Cockrum <ivan@cockrumville.com>
2026-02-09 18:54:06 -0800

    add sample images

--- canvascycle/scenes.js
+++ canvascycle/scenes.js
@@ -1,6 +1,154 @@
 var scenes = [
 	{
-		name: 'TESTRAMP', 
-		title: 'Free Test Image',
-	}
+		name: "TESTRAMP",
+		title: "Free Test Image",
+	},
+	{
+		name: "seascape-day",
+		title: "Seascape - Day",
+	},
+	{
+		name: "V11AM.LBM",
+		title: "Approaching Storm - Day",
+	},
+	{
+		name: "V29FOG.LBM",
+		title: "Seascape - Fog",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
+	{
+		name: "",
+		title: "",
+	},
 ];
