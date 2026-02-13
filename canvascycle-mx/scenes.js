b6d7cbb0730b0ac2175427c3b6612661e9f4632b
Ivan Cockrum <ivan@cockrumville.com>
2026-02-12 20:51:32 -0800

    update scenes

--- canvascycle-mx/scenes.js
+++ canvascycle-mx/scenes.js
@@ -1,5 +1,7 @@
 var scenes = [
+	{ name: "TESTRAMP", title: "Test Image" },
+	{ name: "windswept", title: "Windswept" },
 	{ name: "desert", title: "Desert" },
 	{ name: "skullsorted", title: "Skull Sorted" },
-	{ name: "unpixellatedskull", title: "Unpixellated Skull" }
+	{ name: "unpixellatedskull", title: "Unpixellated Skull" },
 ];
